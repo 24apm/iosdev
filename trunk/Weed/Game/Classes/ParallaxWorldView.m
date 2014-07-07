e70096d82e831317ff9cdea561083804289a9b73
apm24 <amwan04@gmail.com>
2014-07-06 23:04:15 -0700

    update

--- trunk/Weed/Game/Classes/ParallaxWorldView.m
+++ trunk/Weed/Game/Classes/ParallaxWorldView.m
@@ -10,49 +10,11 @@
 #import "ParallaxBackgroundView.h"
 #import "ParallaxForegroundView.h"
 
-@interface PassThroughView : UIScrollView
-
-@end
-
-@implementation PassThroughView
-
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
-    
-    // If not dragging, send event to next responder
-    if (!self.dragging){
-        [self.nextResponder touchesBegan: touches withEvent:event];
-    }
-    else{
-        [super touchesBegan: touches withEvent: event];
-    }
-}
--(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
-    
-    // If not dragging, send event to next responder
-    if (!self.dragging){
-        [self.nextResponder touchesMoved: touches withEvent:event];
-    }
-    else{
-        [super touchesMoved: touches withEvent: event];
-    }
-}
--(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
-    
-    // If not dragging, send event to next responder
-    if (!self.dragging){
-        [self.nextResponder touchesEnded: touches withEvent:event];
-    }
-    else{
-        [super touchesEnded: touches withEvent: event];
-    }
-}
-
-@end
-
 @interface ParallaxWorldView()
 
 @property (nonatomic, strong) NSMutableArray *parallaxViews;
-@property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UIView *scrollView;
+@property (nonatomic) CGFloat lastScreenXOffset;
 
 @end
 
@@ -73,37 +35,97 @@
     ParallaxForegroundView *foregroundView = [[ParallaxForegroundView alloc] init];
     [self.parallaxViews addObject:foregroundView];
     
-    self.scrollView = [[UIScrollView alloc] init];
+    self.scrollView = [[UIView alloc] init];
     [self addSubview:self.scrollView];
 
-    self.scrollView.frame = self.frame;
+    self.scrollView.frame = foregroundView.frame;
     self.scrollView.autoresizingMask = foregroundView.autoresizingMask;
-    self.scrollView.delegate = self;
-    self.scrollView.bounces = NO;
-    self.scrollView.contentSize = foregroundView.frame.size;
     
     for (UIView *view in self.parallaxViews) {
         [self addSubview:view];
     }
     
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)];
+    
+    [self addGestureRecognizer:panGesture];
 
-}
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-    CGFloat percentageOffsetX = scrollView.contentOffset.x / (scrollView.contentSize.width - self.width);
-    for (UIView *view in self.parallaxViews) {
-        view.x = -(view.width - self.width) * percentageOffsetX ;
-    }
 }
 
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
     return [self overlapHitTest:point withEvent:event];
-  //  return [super hitTest:point withEvent:event];
 }
 
-//- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-//    [super touchesBegan:touches withEvent:event];
-//    [self.nextResponder touchesBegan:touches withEvent:event];
-//}
+-(IBAction)handlePanGesture:(UIPanGestureRecognizer *)recognizer {
+    
+    if (recognizer.state == UIGestureRecognizerStateBegan) {
+        self.lastScreenXOffset = self.scrollView.x;
+        
+    } else if (recognizer.state == UIGestureRecognizerStateChanged) {
+        
+        CGPoint translation = [recognizer translationInView:self];
+        NSLog(@"translation %@", NSStringFromCGPoint(translation));
+        self.scrollView.x = self.lastScreenXOffset - translation.x;
+
+        NSLog(@"translation %f", self.scrollView.x);
+
+        if (self.scrollView.x > self.scrollView.width - self.width) {
+            self.scrollView.x = self.scrollView.width - self.width;
+        }
+        
+        if (self.scrollView.x < 0) {
+            self.scrollView.x = 0;
+        }
+        
+        CGFloat percentageOffsetX = self.scrollView.x / (self.scrollView.width - self.width);
+        
+        for (UIView *view in self.parallaxViews) {
+            view.x = -(view.width - self.width) * percentageOffsetX ;
+        }
+        
+    } else if (recognizer.state == UIGestureRecognizerStateEnded) {
+        CGPoint velocity = [recognizer velocityInView:self];
+        CGFloat cappedVelocity = self.width * 2.f;
+        if (velocity.x < -cappedVelocity) {
+            velocity.x = -cappedVelocity;
+        }
+        
+        if (velocity.x > cappedVelocity) {
+            velocity.x = cappedVelocity;
+        }
+        
+        CGFloat magnitude = sqrtf((velocity.x * velocity.x) + (velocity.y * velocity.y));
+        CGFloat slideMult = magnitude / 200;
+        
+        float slideFactor = 0.5 * slideMult; // Increase for more of a slide
+
+        CGFloat transformDifference = (velocity.x * slideFactor);
+        self.scrollView.x -= transformDifference;
+        
+        CGFloat duration = slideFactor;
+        CGFloat xOverflowed;
+        
+        if (self.scrollView.x > self.scrollView.width - self.width) {
+            xOverflowed = self.scrollView.x - self.scrollView.width - self.width;
+            duration *= (xOverflowed - transformDifference) / transformDifference;
+            self.scrollView.x = self.scrollView.width - self.width;
+        }
+        
+        if (self.scrollView.x < 0) {
+            xOverflowed = 0 - self.scrollView.x;
+            duration *= (xOverflowed - transformDifference) / transformDifference;
+
+            self.scrollView.x = 0;
+        }
+        duration = fabsf(MAX(MIN(duration, 0.5f), 1.0f));
+        
+        CGFloat percentageOffsetX = self.scrollView.x / (self.scrollView.width - self.width);
+
+        [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionCurveEaseOut | UIViewKeyframeAnimationOptionBeginFromCurrentState animations:^{
+            for (UIView *view in self.parallaxViews) {
+                view.x = -(view.width - self.width) * percentageOffsetX ;
+            }
+        } completion:nil];
+    }
+}
 
 @end
